f8826b835ed4a40d9946f7fa1b6e0ae5af4c31b3
Ali Hatami <a.hatam008@gmail.com>
2023-03-30 18:14:15 +0430

    add ip_port_type utility

---
+++ util/__init__.py
@@ -0,0 +1,1 @@
+from .common import ip_port_type
---
+++ util/common.py
@@ -0,0 +1,26 @@
+import argparse
+import ipaddress
+
+def ip_port_type(val: str):
+    vals = val.split(":")
+    if len(vals) != 2:
+        raise argparse.ArgumentTypeError(
+            'ip:port error (e.g. 127.0.0.1:12345)')
+    if not vals[1].isdigit():
+        raise argparse.ArgumentTypeError(
+            f'invalid port ({vals[1]}). port value must be digits.')
+    port = int(vals[1])
+    if port > 65536 or port < 0:
+        raise argparse.ArgumentTypeError(
+            f'invalid port ({port}). port number must be in range of [0, 65536].')
+    if vals[0] == '':
+        return ('', port)
+    try:
+        ip = ipaddress.ip_address(vals[0])
+        if ip.version != 4:
+            raise argparse.ArgumentTypeError(
+                f'invalid ip ({vals[0]}). use valid IPv4 address (e.g. 127.0.0.1).')
+        return (vals[0], vals[1])
+    except:
+        raise argparse.ArgumentTypeError(
+            f'invalid ip ({vals[0]}). use valid IPv4 address (e.g. 127.0.0.1).')
